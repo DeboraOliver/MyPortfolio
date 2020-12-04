4af08ebcc20d13a67936b04b7152270c3e057652
Debora Oliveira <debora.ufabc@gmail.com>
2020-12-04 17:11:30 -0300

    Updated using django-filter

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name= 'core'
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('list/', views.post_list, name='post'),
+    #path('jobs/', views.post_list, name='post'),
+    #path('jobs/<int:stack>', views.post, name='post'),
+    path('job/<str:stack>/', views.post, name='post'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,20 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404, redirect
 from .filters import PostFilter
 from .models import Post
 
 # Create your views here.
+# def home(request):
+#     posts = Post.objects.all()
+#     stack = request.GET.get('stack')
+#     if stack:
+#         posts = Post.objects.filter(stack=stack)
+#     return render(request, 'home.html', {'posts' : posts})
+
 def home(request):
     posts = Post.objects.all()
-    stack = request.GET.get('stack')
-    if stack:
-        posts = Post.objects.filter(stack=stack)
-    return render(request, 'home.html', {'posts' : posts})
-
-
-# def post(request, post_id):
-#     post = Post.objects.get(pk=post_id)
-#     return render(request, 'post.html', {'post':post})
-
-def post_list(request):
-    job_list = PostFilter(request.GET, queryset=Post.objects.all())
+    job_list = PostFilter(request.GET, queryset=posts)
     return render(request, 'job.html', {'filter': job_list})
+
+
+def post(request, stack):
+    post = get_object_or_404(Post, stack=stack)
+    return render(request, 'post.html', {'post':post})
+#
+# def post_list(request):
+#     job_list = PostFilter(request.GET, queryset=Post.objects.all())
+#     return render(request, 'job.html', {'filter': job_list})
